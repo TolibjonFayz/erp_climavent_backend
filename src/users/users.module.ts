2fdb1c4bf7e27226ed77592effced74cbb2fdbe6
Tolibjon <tolibjonfayz@gmail.com>
2026-09-25 15:47:19 +0500

    fix(di): Export SequelizeModule and JwtModule from global UsersModule to resolve Guard dependencies

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import { JwtModule } from '@nestjs/jwt';
 
+@Global()
 @Module({
   imports: [SequelizeModule.forFeature([User]), JwtModule.register({})],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService],
+  exports: [UsersService, SequelizeModule, JwtModule],
 })
 export class UsersModule {}
